b3c26767d426377957eb19d2730b94587d4d07cb
Nicholas Perry <nicholasperrypro@gmail.com>
2021-06-29 18:32:36 -0400

    Add project files.

---
+++ PairProgrammingTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairProgrammingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string directoryPath = Directory.GetParent(workingDirectory).Parent.FullName;
+            // 
+            var fileReader = new StreamReader($"{directoryPath}/game-sequence.txt");
+            var line = fileReader.ReadLine();
+            var events = line.Split(',');
+        }
+    }
+}
